cce81cd84de0342b603245e81b96c2929a9fcf66
jithinksunil <jithinksunil96@gmail.com>
2025-04-05 21:03:13 +0530

    swagger configuration completed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
+  @ApiOperation({
+    summary: 'Sample route',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Return a plain text',
+    schema: { type: 'string', example: 'Hello World!' },
+  })
   getHello(): string {
     return this.appService.getHello();
   }
