63c8461e813d4d2798ce03b3bcb4d7fae3e951b8
jithinksunil <jithinksunil96@gmail.com>
2025-04-05 20:32:42 +0530

    added global exception handling with logger for better debugging

---
+++ src/common/constants.ts
@@ -0,0 +1,8 @@
+export const serverPrefix = '/api';
+
+export enum ANSI_COLORS {
+  RED = '\x1b[31m',
+  GREEN = '\x1b[32m',
+  YELLOW = '\x1b[33m',
+  RESET = '\x1b[0m',
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { serverPrefix } from './common/constants';
+import { GlobalExceptionFilter } from './globalException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix(serverPrefix);
+  app.enableCors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  });
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
