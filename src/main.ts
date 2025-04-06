3a22d55ddccd3cee8382790361b58db5fe2ab04c
jithinksunil <jithinksunil96@gmail.com>
2025-04-06 10:12:49 +0530

    cookie parser added

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './globalException.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { serverPrefix } from './common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,12 +15,13 @@ async function bootstrap() {
     origin: process.env.FRONTEND_URL,
   });
   const config = new DocumentBuilder()
-    .setTitle('Rag-Q&A API')
-    .setDescription('Api for users and document management')
-    .setVersion('1.0')
-    .build();
+  .setTitle('Rag-Q&A API')
+  .setDescription('Api for users and document management')
+  .setVersion('1.0')
+  .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
+  app.use(cookieParser());
   app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
